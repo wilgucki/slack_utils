f0fc40e4152dbef867a28d0a61f70fb9f6fc57d0
Maciej Wilgucki <mwilgucki@gmail.com>
2020-01-02 19:57:23 +1100

    minor setup.py adjustments

--- setup.py
+++ setup.py
@@ -17,16 +17,15 @@ setup_requirements = ['pytest-runner', ]
 test_requirements = ['pytest>=3', ]
 
 setup(
-    author="mac",
+    author="kopytko",
     author_email='mwilgucki@gmail.com',
     python_requires='>=3.5',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
