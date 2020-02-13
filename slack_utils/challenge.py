324533ad6a13cfe7c9c414cd98bf5a533c2bc6d6
Maciej Wilgucki <mwilgucki@gmail.com>
2020-02-13 21:31:59 +1100

    docs

--- slack_utils/challenge.py
+++ slack_utils/challenge.py
@@ -5,11 +5,22 @@ from slack_utils.exceptions import SlackException
 
 
 def verify_token(token):
+    """Verifies Slack verification token
+
+    :param token: Slack verification token
+    """
     if token != os.getenv('SLACK_VERIFICATION_TOKEN'):
         raise SlackException('Invalid verification token')
 
 
 def respond(challenge, token):
+    """Responds to Slack challenge
+
+    :param challenge: Slack challenge string
+    :param token: Slack verification token
+    :returns: Slack challenge string in json format
+    :rtype: dict
+    """
     verify_token(token)
     return {
         'statusCode': 200,
