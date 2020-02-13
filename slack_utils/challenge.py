ba61280645c4cad7ecd13e64041c61adf68c2d67
Maciej Wilgucki <mwilgucki@gmail.com>
2020-02-13 21:20:28 +1100

    slack challenge

---
+++ slack_utils/challenge.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+from slack_utils.exceptions import SlackException
+
+
+def verify_token(token):
+    if token != os.getenv('SLACK_VERIFICATION_TOKEN'):
+        raise SlackException('Invalid verification token')
+
+
+def respond(challenge, token):
+    verify_token(token)
+    return {
+        'statusCode': 200,
+        'body': json.dumps({
+            'challenge': challenge
+        }),
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
